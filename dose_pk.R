ecac801c552608b56383581c61aeeb56c792192a
Yousuf Ali <md.ali@fda.hhs.gov>
2021-12-02 15:19:33 -0500

    new files added

---
+++ dose_pk.R
@@ -0,0 +1,151 @@
+library(shiny)
+library(DBI)
+library(data.table)
+library(RSQLite)
+source("get_dose_pp.R")
+
+
+db_path <- "C:/Users/Md.Ali/OneDrive - FDA/Documents/00_projects/04_project_sendexplorer_DB/CDER_SEND.db"
+
+conn <-RSQLite::dbConnect(drv = SQLite(),db_path)
+sd_id <- RSQLite::dbGetQuery(conn=conn, 'SELECT DISTINCT STUDYID FROM TX')
+sd_id <- data.table::as.data.table(sd_id)
+#names(sd_id) <- ""
+#SELECT DISTINCT PPTESTCD FROM PP WHERE STUDYID=='8001964' AND PPTESTCD LIKE '%auc%'
+#RSQLite::dbGetQuery(conn = conn, 'SELECT * FROM POOLDEF WHERE STUDYID==:x', params = list(x=studyid))
+
+
+ui <- shiny::fluidPage(
+  title = "Dose and PK automation",
+  shiny::titlePanel("dose and pk value extraction"),
+  
+  shiny::sidebarLayout(
+    
+    shiny::sidebarPanel(
+      shiny::selectizeInput(inputId = "studyid",
+                         label= "Select StudyID",
+                         selected = NULL,
+                         choices = NULL), 
+      shiny::uiOutput("choose_auc"),
+
+      
+      shiny::uiOutput("dose")
+    ),
+    shiny::mainPanel(
+      
+      shiny::tableOutput("doseTable")
+      
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+ 
+  
+  updateSelectizeInput(session = session, inputId = 'studyid', choices = c(Choose = '', sd_id),
+                       server = TRUE,
+                       options = list(maxOptions = 5000))
+  
+
+  output$choose_auc <- shiny::renderUI({
+    
+    auc_list <- RSQLite::dbGetQuery(conn=conn, 'SELECT DISTINCT PPTESTCD FROM PP WHERE STUDYID=:x AND PPTESTCD LIKE "%auc%"',
+                                    params=list(x=input$studyid))
+    # print(input$studyid)
+    # print("----")
+   
+    auc_list <- data.table::as.data.table(auc_list)
+    #print(auc_list)
+ 
+
+    shiny::selectizeInput(inputId = "auc", 
+                          label="Select AUC parameter",
+                          selected= NULL,
+                          choices= c(Choose="", auc_list))
+  })
+  
+
+  
+  
+  
+  get_dose_pk_for_study <- shiny::reactive({
+    
+    if (!is.null(input$studyid) & !is.null(input$auc)) {
+    df <- get_pk_param(conn=conn, input$studyid, pk_param = input$auc )
+    df
+    }
+    
+    
+  })
+  
+  
+  
+  output$dose <- renderUI({
+    df <- get_dose_pk_for_study()
+    n_dose <-   length(unique(df[,TRTDOS]))
+
+    cmax <- df[PPTESTCD=="CMAX"]
+    auc <- df[PPTESTCD!="CMAX"]
+    # 
+    # for (i in seq_along(1:3)) {
+    #   dose <- cmax[i,.(TRTDOS)]
+    #   cmax_value <- cmax[i,  .(mean)]
+    #   auc_value <- auc[i,.(mean)]
+    #   print(dose)
+    #   print(cmax_value)
+    #   print((auc_value))
+    # }
+    
+    # lapply(1:n_dose, function(i) {
+    #   div(style="display: inline-block;vertical-align:top; width: 115px;",
+    #   shiny::numericInput(inputId = paste0(input$auc,i), label = paste0(input$auc,i), value = auc[i,.(mean)]))
+    #   
+    # })
+    
+    lapply(1:(4*n_dose), function(i) {
+      I <- ceiling(i/4)
+      #doseName <- names(studyData$Doses)[I]
+      if (i %% 4 == 1) {
+        div(hr(style = "border-top: 1px dashed skyblue"),
+            numericInput(paste0('dose',I),paste0('*Dose ',I,  " ",cmax[I, .(TRTDOSU)], ":"), min=0, value = cmax[I, .(TRTDOS)]))
+      } else if (i %% 4 == 2) {
+        div(style="display: inline-block;vertical-align:top; width: 115px;",
+            numericInput(paste0('Cmax',I),paste0('Cmax ',I, " ", cmax[I, .(PPSTRESU)], ":"), min=0, value=cmax[I, .(mean)]))
+      }
+      else if (i %% 4 == 3) {
+        div(style="display: inline-block;vertical-align:top; width: 115px;",
+            numericInput(paste0("AUC",I),paste0(input$auc, " ",I, " ",auc[I, PPSTRESU], ":"), min=0, value=auc[I, .(mean)]))
+        
+      }
+      # else {
+      #   div(checkboxInput(paste0('NOAEL',I),'NOAEL?',value= T))
+      # }
+    })
+    
+    
+    
+  })
+  
+output$doseTable <- shiny::renderTable({
+  req(input$studyid)
+  req(input$auc)
+  df <- get_dose_pk_for_study()
+  df
+  
+  
+})
+  
+  
+
+shiny::onSessionEnded(function() {
+  RSQLite::dbDisconnect(conn = conn)
+})
+
+}
+
+shiny::shinyApp(ui,server)
+
+
+# https://stackoverflow.com/questions/38438920/shiny-selectinput-very-slow-on-larger-data-15-000-entries-in-browser
---
+++ get_dose_pp.R
@@ -0,0 +1,142 @@
+
+# function to get dose and Pharmacokinetics parameter for a specific study
+# library(DBI)
+# library(data.table)
+
+#' Title
+#'
+#' @param db 
+#' @param studyid 
+#' @param pk_param 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_pk_param <- function(conn, studyid, pk_param="AUCLST"){
+  
+  
+  '%ni%' <- Negate('%in%')
+  
+  studyid <- as.character(studyid)
+  pk_param <- toupper(as.character(pk_param))
+  studyid_list_all <- RSQLite::dbGetQuery(conn = conn,
+                                          'SELECT DISTINCT STUDYID FROM TX')
+  studyid_list_pp <- RSQLite::dbGetQuery(conn = conn,
+                                         'SELECT DISTINCT STUDYID FROM PP')
+  testcode_list_pp <- RSQLite::dbGetQuery(conn = conn,
+                                          'SELECT DISTINCT PPTESTCD FROM PP WHERE STUDYID==:x',
+                                          params=list(x=studyid))
+  
+  if (studyid %ni% studyid_list_all$STUDYID) {
+    stop("Study not found in the database")
+  }
+  if (studyid %ni% studyid_list_pp$STUDYID) {
+    stop("Study does not contain PP domain")
+  }
+  if (pk_param %ni% testcode_list_pp$PPTESTCD) {
+    stop(paste0(pk_param, " is not found in PPTESTCD variable in PP domain for ", studyid, " study"))
+  }
+  
+  pp_domain <- RSQLite::dbGetQuery(conn = conn,
+                                   'SELECT * FROM PP WHERE STUDYID==:x AND PPTESTCD IN ($CMAX, $AUC)',
+                                   params = list(x=studyid,CMAX="CMAX", AUC=pk_param))
+  
+  # get the dose information
+  dose <- RSQLite::dbGetQuery(conn=conn, 'SELECT TX.STUDYID, TX.TXPARMCD, TX.TXVAL, TX.SETCD,
+           DM.USUBJID FROM TX INNER JOIN DM ON (TX.STUDYID=DM.STUDYID AND TX.SETCD=DM.SETCD)
+           WHERE TX.STUDYID==:x AND (TX.TXPARMCD="TRTDOS" OR TX.TXPARMCD="TRTDOSU")',
+           params=list(x=studyid))
+  
+  dose <- data.table::as.data.table(dose)
+  dose_wide <- data.table::dcast.data.table(dose, ... ~ TXPARMCD,
+                                            value.var = "TXVAL")
+  dose_wide$TRTDOS <- clean_txval_dose(dose = dose_wide$TRTDOS)
+  #dose_wide$TRTDOS <- as.numeric(dose_wide$TRTDOS)
+  # select only required columns
+  pp_domain <- data.table::as.data.table(pp_domain)
+  pp_domain <-  pp_domain[, .(STUDYID, DOMAIN,USUBJID,POOLID,PPTESTCD,PPTEST,
+                              PPORRES,PPORRESU,PPSTRESC,PPSTRESU,PPSTRESN)]
+  
+  #
+  if (nrow(pp_domain) == sum(pp_domain$USUBJID!="")) {
+    df <- merge(dose_wide,pp_domain, by=c("STUDYID","USUBJID"))
+    # print("USUBJID is populated in PP")
+    df <- df[, .(TRTDOS,TRTDOSU, PPSTRESN, PPSTRESU,PPTESTCD )]
+    df <- df[, .(mean=mean(PPSTRESN)), by=.(TRTDOS, TRTDOSU,PPTESTCD,PPSTRESU)]
+    #names(df)[5] <- paste0(pk_param, "_Mean")
+    df <- df[, .SD, .SDcols=c(1,2,3,5,4)]
+    
+  } else if (nrow(pp_domain) == sum(pp_domain$USUBJID=="")) {
+    pooldef_list <- RSQLite::dbGetQuery(conn = conn, 'SELECT DISTINCT STUDYID FROM POOLDEF')
+    if (studyid %ni% pooldef_list$STUDYID) {
+      stop("USUBJID in PP domain empty and this study does not have POOLDEF domain to merge with PP domain")
+    }
+    pooldef <- RSQLite::dbGetQuery(conn = conn, 'SELECT * FROM POOLDEF WHERE STUDYID==:x', params = list(x=studyid))
+    pooldef <- data.table::as.data.table(pooldef)
+    pp_domain <- pp_domain[, USUBJID := NULL]
+    df <- data.table::merge.data.table(pp_domain, pooldef, by=c("STUDYID", "POOLID"),allow.cartesian=T)
+    df <- data.table::merge.data.table(df,dose_wide, by=c("STUDYID","USUBJID"))
+    df <- df[, .(TRTDOS,TRTDOSU, PPSTRESN, PPSTRESU,PPTESTCD)]
+    df <- df[, .(mean=mean(PPSTRESN)), by=.(TRTDOS, TRTDOSU,PPTESTCD,PPSTRESU)]
+    #names(df)[5] <- paste0(pk_param, "_Mean")
+    df <- df[, .SD, .SDcols=c(1,2,3,5,4)]
+    
+  } else {
+    # filter only poolid populated
+    poolid_pp <- pp_domain[POOLID!="", ]
+    poolid_pp <- poolid_pp[, USUBJID:= NULL]
+    #filter only usubjid populated
+    usubjid_pp <- pp_domain[USUBJID!=""]
+    usubjid_pp <- usubjid_pp[, POOLID:=NULL]
+    # get dose info for poolid 
+    pooldef <- RSQLite::dbGetQuery(conn = conn, 'SELECT * FROM POOLDEF WHERE STUDYID==:x',
+                                   params = list(x=studyid))
+    pooldef <- data.table::as.data.table(pooldef)
+    df <- data.table::merge.data.table(poolid_pp, pooldef,
+                                       by=c("STUDYID", "POOLID"),
+                                       allow.cartesian = T)
+    df <- data.table::merge.data.table(df,dose_wide,
+                                       by=c("STUDYID","USUBJID"),
+                                       allow.cartesian = TRUE)
+    
+    df <- df[, POOLID:=NULL]
+    # get dose info for usubjid
+    df_usubjid <- data.table::merge.data.table(usubjid_pp,dose_wide,
+                                               by=c("STUDYID","USUBJID"),
+                                               allow.cartesian = TRUE)
+    
+    row_bind <- list(df,df_usubjid)
+    df_all <- data.table::rbindlist(row_bind, use.names = T)
+    # check duplicated value dataframe
+    df <- df_all[, .(mean_cmax=mean(PPSTRESN)), by=.(TRTDOS, TRTDOSU,PPTESTCD,PPSTRESU)]
+    #names(df)[5] <- paste0(pk_param, "_Mean")
+    df <- df[, .SD, .SDcols=c(1,2,3,5,4)]
+  }
+  
+  df <- df[order(PPTESTCD,TRTDOS),]
+  
+  return(df)
+}
+
+
+
+
+clean_txval_dose <- function(dose) {
+  index <- 1:length(dose)
+  dose_char <- dose
+  dose_num <- as.numeric(dose)
+  final_dose <- NA
+  
+  for (i in index) {
+    if (is.na(dose_num[i])) {
+      x <- gsub(";|-|\\/|\\|", ",", dose_char[i])
+      x <- max(as.numeric(unlist(strsplit(x,","))))
+      final_dose[i] <- x
+      
+    } else {
+      final_dose[i] <- dose_num[i]
+    }
+  }
+  return(final_dose)
+}
