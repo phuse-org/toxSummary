dd88cb5920d74df9bbd61124951f926232a929f4
Yousuf Ali <yousuf.pharma@gmail.com>
2022-08-11 07:35:20 -0400

    format

--- database_metadata.R
+++ database_metadata.R
@@ -1,5 +1,95 @@
+
+
+#############################################################################
+# create metadata table                                                     #
+#############################################################################
+
+library(RSQLite)
+library(data.table)
+
+#  connect database
+db_path <- "C:/Users/Md.Ali/OneDrive - FDA/yousuf/10_DATA/SEND_unclean.db"
+conn <- RSQLite::dbConnect(drv = SQLite(), db_path)
+ind_table <- data.table::as.data.table(RSQLite::dbGetQuery(
+    conn = conn, "SELECT *  FROM ID"
+))
+ind_table <- ind_table[APPID %like% "IND", ]
+ind_table <- ind_table[1:1000]
+# get_pp <- function(conn, studyid) {
+
+#  df <- RSQLite::dbGetQuery(conn = conn,
+#   'SELECT * FROM PP WHERE STUDYID==:x',
+#   params = list(x = studyid))
+#   df <- data.table::as.data.table(df)
+#   df
+
+# }
+
+pp <- RSQLite::dbGetQuery(
+    conn = conn,
+    "SELECT *  FROM PP "
+)
+
+
+num_row <- NA
+USUBJID <- NA
+POOLID <- NA
+PPTESTCD <- list()
+VISITDY_populated <- NA
+PPNOMDY_populated <- NA
+
+
+for (i in 1:nrow(ind_table)) {
+    studyid <- ind_table[["STUDYID"]][i]
+    df <- pp[STUDYID == studyid]
+    num_row[i] <- nrow(df)
+    if (nrow(df) != 0) {
+        USUBJID[i] <- ifelse(nrow(df) == sum(df$USUBJID == ""), "empty", ifelse(
+            nrow(df) == sum(df$USUBJID != ""), "populated",
+            ifelse(all(is.na(df$USUBJID)), NA, "mixed")
+        ))
+        POOLID[i] <- ifelse(nrow(df) == sum(df$POOLID == ""), "empty", ifelse(
+            nrow(df) == sum(df$POOLID != ""), "populated",
+            ifelse(all(is.na(df$POOLID)), NA, "mixed")
+        ))
+        PPTESTCD[[i]] <- unique(df[["PPTESTCD"]])
+        VISITDY_populated[i] <- ifelse(all(is.na(df[["VISITDY"]])), FALSE, TRUE)
+        PPNOMDY_populated[i] <- ifelse(all(is.na(df[["PPNOMDY"]])), FALSE, TRUE)
+    } else {
+        USUBJID[i] <- "no observation"
+        POOLID[i] <- "no observation"
+        PPTESTCD[i] <- NA
+        VISITDY_populated[i] <- NA
+        PPNOMDY_populated[i] <- NA
+    }
+}
+
+
+final_df <- data.table::copy(ind_table)
+final_df[, `:=`(
+	num_row = num_row,
+	usubjid = USUBJID,
+	poolid = POOLID,
+	visitdy_populated = VISITDY_populated,
+	ppnomdy_populated = PPNOMDY_populated,
+	unq_testcd = PPTESTCD
+)]
+
+# end
+
+
+#######################################################
+
+
+get_pk_param(conn= conn, "AP203", pk_param = "AUCLST")
+	# debugonce(get_pk_param)
+#######################################################################
+# exploratory
+######################
+
 # load package
 library(RSQLite)
+library(data.table)
 source("get_dose_pp.R")
 
 #  connect database
@@ -31,91 +121,11 @@ dt[STUDYID=="2769-005"]
 dt[STUDYID=="2.917E+11"]
 
 
-
+tictoc::tic()
 pp <- RSQLite::dbGetQuery(
     conn = conn,
-    "SELECT *  FROM PP  WHERE STUDYID='2688-006'"
+    "SELECT *  FROM PP "
 )
-
-#############################################################################
-ind_table <- data.table::as.data.table(RSQLite::dbGetQuery(
-    conn = conn, "SELECT *  FROM ID"
-))
-ind_table <- ind_table[APPID %like% "IND", ]
-ind_table <- ind_table[1:100]
-get_pp <- function(conn, studyid) {
-
- df <- RSQLite::dbGetQuery(conn = conn,
-  'SELECT * FROM PP WHERE STUDYID==:x',
-  params = list(x = studyid))
-  df <- data.table::as.data.table(df)
-  df
-
-}
-
-
-num_row <- NA
-USUBJID <- NA
-POOLID <- NA
-PPTESTCD <- list()
-VISITDY_populated <- NA
-PPNOMDY_populated <- NA
-
-tictoc::tic()
-for (i in 1:nrow(ind_table)) {
-	studyid <- ind_table[["STUDYID"]][i]
-	df <- get_pp(conn, studyid)
-	num_row [i]  <- nrow(df)
-	if (nrow(df) != 0) {
-	USUBJID[i] <- ifelse(nrow(df) == sum(df$USUBJID == ""), "empty", ifelse(
-		nrow(df) == sum(df$USUBJID != ""), "populated", ifelse(all(is.na(df$USUBJID)), NA, "mixed")
-	))
-
-	POOLID[i] <- ifelse(nrow(df) == sum(df$POOLID == ""), "empty", ifelse(
-		nrow(df) == sum(df$POOLID != ""), "populated", ifelse(all(is.na(df$POOLID)), NA, "mixed")
-	))
-
-
-	PPTESTCD[[i]] <- unique(df[["PPTESTCD"]])
-	VISITDY_populated[i] <- ifelse(all(is.na(df[["VISITDY"]])), FALSE, TRUE)
-	PPNOMDY_populated[i] <- ifelse(all(is.na(df[["PPNOMDY"]])), FALSE, TRUE)
-	} else {
-	USUBJID[i] <- "no observation"
-	POOLID[i] <- "no observation"
-	PPTESTCD[i] <- NA
-	VISITDY_populated[i] <- NA
-	PPNOMDY_populated[i] <- NA
-
-	}
-
-}
-
 tictoc::toc()
 
-final_df <- data.table::copy(ind_table)
-final_df[, `:=`(
-	num_row = num_row,
-	usubjid = USUBJID,
-	poolid = POOLID,
-	visitdy_populated = VISITDY_populated,
-	ppnomdy_populated = PPNOMDY_populated,
-	unq_testcd = PPTESTCD
-)]
-#######################################################
-ind_table[, num_row_co_2 := num_row]
-ind_table
-num_row
-USUBJID
-POOLID
-PPTESTCD
-VISITDY_populated
-
-PPNOMDY_populated
-
-df <- get_pp(conn, "AR037")
-is.na(df[["USUBJID"]])
-
-head(df	)
-
-get_pk_param(conn= conn, "AP203", pk_param = "AUCLST")
-	# debugonce(get_pk_param)
+pp <- data.table::as.data.table(pp)
